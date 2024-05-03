efc4c74dd21dfca0f4cdd189661703ed22cc6f12
kakiuchi-shigenao <87973394+endou-mame@users.noreply.github.com>
2024-05-03 17:45:23 +0000

    initial commit Fixes #2

---
+++ src/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EndouMame\PhpMonad;
+
+/**
+ * テスト
+ */
+final readonly class Test
+{
+    public function __construct(
+        public string $id
+    ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+}
